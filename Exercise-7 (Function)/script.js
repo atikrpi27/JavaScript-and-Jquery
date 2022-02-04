60551d765a578822aa5c8086e529d8c3b0a8d21a
atikrpi27 <atik.rpi27@gmail.com>
2022-02-04 20:42:48 +0600

    Funtion

---
+++ Exercise-7 (Function)/script.js
@@ -0,0 +1,28 @@
+//create function
+function atik(){
+    var num = 5;
+    var result = num*num;
+    document.write("Square of 5 is: " +result + "<br>")
+}
+
+atik();
+
+//passing peramitter
+function add(num1,num2) {
+    var num1, num2;
+    add = num1 + num2; 
+    document.write("Addition of " + num1 + " and " + num2 + " is: " + add + "<br>")
+}
+add(10,12);
+
+//input from user
+function userInput(number1,number2) {
+    var number1,number2;
+    number1 = parseInt(prompt("Enter Your First Number: "))
+    number2 = parseInt(prompt("Enter Your Second Number: "))
+    res = parseInt(number1 + number2); 
+    // document.write("Addition of " + number1 + " and " + number2 + " is: " + res + "<br>")
+    return res
+}
+
+document.write("Addition of  number1  and  number2 is: " + userInput());
