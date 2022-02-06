afd207893a3d95d0148525012665b12bfc746be2
atikrpi27 <atik.rpi27@gmail.com>
2022-02-06 22:53:17 +0600

    Positive or Negative number using ternary operator in a function

--- Exercise-7 (Function)/script.js
+++ Exercise-7 (Function)/script.js
@@ -1,48 +1,37 @@
 //create function
-function atik(){
+function atik() {
     var num = 5;
-    var result = num*num;
-    document.write("Square of 5 is: " +result + "<br>")
+    var result = num * num;
+    document.write("Square of 5 is: " + result + "<br>")
 }
 
 atik();
 
 //passing peramitter
-function add(num1,num2) {
+function add(num1, num2) {
     var num1, num2;
-    add = num1 + num2; 
+    add = num1 + num2;
     document.write("Addition of " + num1 + " and " + num2 + " is: " + add + "<br>")
 }
-add(10,12);
+add(10, 12);
 
 //input from user
-function userInput(number1,number2) {
-    var number1,number2;
+function userInput(number1, number2) {
+    var number1, number2;
     number1 = parseInt(prompt("Enter Your First Number: "))
     number2 = parseInt(prompt("Enter Your Second Number: "))
-    res = parseInt(number1 + number2); 
+    res = parseInt(number1 + number2);
     // document.write("Addition of " + number1 + " and " + number2 + " is: " + res + "<br>")
     return res
 }
 
 document.write("Addition of  number1  and  number2 is: " + userInput());
 
-//passing peramitter
-function add(num1,num2) {
-    var num1, num2;
-    add = num1 + num2; 
-    document.write("Addition of " + num1 + " and " + num2 + " is: " + add + "<br>")
+//positive or negative
+//using userInput function number1 variable
+function test() {
+    number1 = parseInt(prompt("Enter Your Test Number: "))
+    test = number1 > 0 ? 'Your number is positive.' : 'Your number is negative.'
+    document.write("<br>" + test)
 }
-add(10,12);
-
-//input from user
-function userInput(number1,number2) {
-    var number1,number2;
-    number1 = parseInt(prompt("Enter Your First Number: "))
-    number2 = parseInt(prompt("Enter Your Second Number: "))
-    res = parseInt(number1 + number2); 
-    // document.write("Addition of " + number1 + " and " + number2 + " is: " + res + "<br>")
-    return res
-}
-
-document.write("Addition of  number1  and  number2 is: " + userInput());
+test()
