0d64cd875d852fb5a1e9384f6e9e271c35263475
atikrpi27 <atik.rpi27@gmail.com>
2022-02-11 23:56:38 +0600

    Loop in Array & Sum of Array

--- Exercise-8 (Array)/script.js
+++ Exercise-8 (Array)/script.js
@@ -33,4 +33,24 @@ document.write("Length of country1: " + country1.length + "<br>");
 var country2 = ["Australia" , "South Africa"];
 document.write("Values of country2: " + country2 + "<br>");
 document.write("Length of country2: " + country2.length + "<br>");
-document.write("Concatenate of 2Arrays: " + country1.concat(country2))
+document.write("Concatenate of 2Arrays: " + country1.concat(country2) + "<br>" + "<br>")
+
+
+//Loop in Array
+
+var num = [1,2,3,4,5,6,7,8,9];
+var sum = 0;
+
+document.write("Values of Array: " + num + "<br>");
+var lengths = parseInt( num.length + "<br>");
+document.write("Length of Array: " + num.length + "<br>");
+document.write("Output of Array using Loop: " + "<br>");
+
+for(i=0; i<lengths; i++){
+    // console.log(num[i]);
+    document.write(num[i] + "<br>");
+    sum += num[i];
+    
+}
+//sum of Array
+document.write("Sum of Array's values: " + sum)
