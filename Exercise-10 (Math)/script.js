f0e003697701589f2d9167e87d8a9d609570b91a
atikrpi27 <atik.rpi27@gmail.com>
2022-02-15 22:44:37 +0600

    Ex-10: Math Functions and Genarate Random Number

---
+++ Exercise-10 (Math)/script.js
@@ -0,0 +1,36 @@
+//Math Function
+document.write("Square(25) Function: " + Math.sqrt(25) + "<br>");
+document.write("ABS(-25) Function: " + Math.abs(-25) + "<br>");
+document.write("Sin(2) Function: " + Math.sin(2) + "<br>");
+document.write("Power(2,3) Function: " + Math.pow(2,3) + "<br>");
+document.write("Floor(2.3) Function: " + Math.floor(2.3) + "<br>");
+document.write("Floor(2.7) Function: " + Math.floor(2.7) + "<br>");
+document.write("ceil(2.2) Function: " + Math.ceil(2.2) + "<br>");
+document.write("ceil(2.7) Function: " + Math.ceil(2.7) + "<br>");
+document.write("Round(4.7) Function: " + Math.round(4.7) + "<br>");
+document.write("Round(4.4) Function: " + Math.round(4.4) + "<br>");
+document.write("Maximum(34,94) Function: " + Math.max(34,94) + "<br>" + "<br>");
+
+
+//Random Number
+document.write("Random Number: " + "<br>");
+document.write("Random Number: " + Math.random() + "<br>");
+document.write("Random Number: " + Math.random() + "<br>");
+document.write("Random Number: " + Math.random() + "<br>" + "<br>");
+
+document.write("Random Number (0-5.999): "+ "<br>");
+document.write("Random Number: " + Math.random() * 6 + "<br>");
+document.write("Random Number: " + Math.random() * 6 + "<br>");
+document.write("Random Number: " + Math.random() * 6 + "<br>" + "<br>");
+
+document.write("Random Number (0-5): "+ "<br>");
+// document.write("Random Number: " + Math.floor(Math.random() * 6) + "<br>");
+document.write("Random Number: " + (Math.floor(Math.random() * 5) + 1) + "<br>");
+document.write("Random Number: " + (Math.floor(Math.random() * 5) + 1) + "<br>");
+document.write("Random Number: " + (Math.floor(Math.random() * 5) + 1) + "<br>" + "<br>");
+
+
+document.write("Random Number (10-20): "+ "<br>");
+document.write("Random Number: " + (Math.floor(Math.random() * 11) + 10) + "<br>");
+document.write("Random Number: " + (Math.floor(Math.random() * 11) + 10) + "<br>");
+document.write("Random Number: " + (Math.floor(Math.random() * 11) + 10) + "<br>");
