60471eaafbc0b5c08f27a926df59bbff645490a8
atikrpi27 <atik.rpi27@gmail.com>
2022-02-17 23:00:38 +0600

    Date object and Date method

---
+++ Exercise-7 (Function)/function.js
@@ -0,0 +1,21 @@
+//Date object and Date method
+
+document.write("<br>" + "<br>" + "Date Function: " + "<br>");
+
+var date = new Date();
+document.write(date + "<br>");
+
+var year = date.getFullYear();
+document.write("Year: " + year + "<br>");
+
+var Month = date.getMonth();
+document.write("Month: " + Month + "<br>");
+
+var Day = date.getDay();
+document.write("Day: " + Day + "<br>");
+
+var Hours = date.getHours();
+document.write("Hours: " + Hours + "<br>");
+
+var Minutes = date.getMinutes();
+document.write("Minutes: " + Minutes + "<br>");
