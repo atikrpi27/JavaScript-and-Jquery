3e25f827600db3b2cb9b361367f257f39f724690
atikrpi27 <atik.rpi27@gmail.com>
2022-02-20 22:47:19 +0600

    Exercise-12 (DOM Select HTML elements)

---
+++ Exercise-12 (DOM Select HTML elements)/script.js
@@ -0,0 +1,7 @@
+//Select by ID
+
+var heading1 = document.getElementById("heading1").innerHTML=("hi");
+// document.write(heading1);
+
+var heading = document.getElementsByid("heading1").innerHTML = ("dhdsfkhdns");
+// document.write(heading2);
