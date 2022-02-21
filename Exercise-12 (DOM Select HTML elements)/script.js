6e17e0e85e0f70c1c2dd527be483e887598e5f53
atikrpi27 <atik.rpi27@gmail.com>
2022-02-21 23:43:11 +0600

    query selector

--- Exercise-12 (DOM Select HTML elements)/script.js
+++ Exercise-12 (DOM Select HTML elements)/script.js
@@ -1,7 +1,7 @@
 //Select by ID
-
-var heading1 = document.getElementById("heading1").innerHTML=("hi");
-// document.write(heading1);
-
-var heading = document.getElementsByid("heading1").innerHTML = ("dhdsfkhdns");
-// document.write(heading2);
+var heading1 = document.getElementById("heading1").innerHTML = ("hi");
+//Select by class
+var heading = document.getElementsByClassName("h6").innerHTML = ("dhdsfkhdns");
+//Select by query selector
+var qSelect = document.querySelector("#pid").innerHTML = ("hikfmd");
+var qSelect = document.querySelector(".pclass").innerHTML = ("hi I am Atik.");
