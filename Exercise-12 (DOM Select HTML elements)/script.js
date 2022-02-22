acc104450eb759b76cb49e6328c1885e94be3b8d
atikrpi27 <atik.rpi27@gmail.com>
2022-02-22 23:48:10 +0600

    Ex-13: Event Handeler

--- Exercise-12 (DOM Select HTML elements)/script.js
+++ Exercise-12 (DOM Select HTML elements)/script.js
@@ -1,7 +1,10 @@
 //Select by ID
 var heading1 = document.getElementById("heading1").innerHTML = ("hi");
+
 //Select by class
 var heading = document.getElementsByClassName("h6").innerHTML = ("dhdsfkhdns");
+
 //Select by query selector
 var qSelect = document.querySelector("#pid").innerHTML = ("hikfmd");
 var qSelect = document.querySelector(".pclass").innerHTML = ("hi I am Atik.");
+
---
+++ Exercise-13 (Event Handeler)/script.js
@@ -0,0 +1,14 @@
+//Event Handeler
+
+function button1(){
+    alert("This is Button 1")
+}
+
+function button2(){
+    alert("This is Button 2")
+}
+
+function button3(){
+    var txt = document.querySelector("#pid");
+    txt.innerHTML = ("You are clicked on Button 3.")
+}
