ab8d5ba2c0d7001d97e7f24aa0e902199cd2fe7b
atikrpi27 <atik.rpi27@gmail.com>
2022-02-24 23:20:26 +0600

    click event input from user using prompt

--- Exercise-13 (Event Handeler)/script.js
+++ Exercise-13 (Event Handeler)/script.js
@@ -13,6 +13,11 @@ function button3(){
     txt.innerHTML = ("You are clicked on Button 3.")
 }
 
+function button4(){
+    var txt1 = document.querySelector("#pid1");
+    txt1.innerHTML = (prompt("Write Your Says: "));
+}
+
 var pic = document.querySelector("#mypic");
 function aimg(){
     pic.src = "atik.jpg";
