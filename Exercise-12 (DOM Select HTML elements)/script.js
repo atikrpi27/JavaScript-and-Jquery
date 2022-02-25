12b01b5cf05ecf2e69b3716ca6109a4827d60a90
atikrpi27 <atik.rpi27@gmail.com>
2022-02-25 23:37:59 +0600

    change line to input data using prompt

--- Exercise-12 (DOM Select HTML elements)/script.js
+++ Exercise-12 (DOM Select HTML elements)/script.js
@@ -6,5 +6,6 @@ var heading = document.getElementsByClassName("h6").innerHTML = ("dhdsfkhdns");
 
 //Select by query selector
 var qSelect = document.querySelector("#pid").innerHTML = ("hikfmd");
-var qSelect = document.querySelector(".pclass").innerHTML = ("hi I am Atik.");
+var qSelect1 = document.querySelector(".pclass").innerHTML = ("hi I am Atik.");
+var qSelect2 = document.querySelector("#prompt").innerHTML = prompt("Write your words: ");
 
