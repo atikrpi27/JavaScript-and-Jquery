c9d28482dbf1c78a0444b6236e51496cbdef245f
atikrpi27 <atik.rpi27@gmail.com>
2022-02-27 23:57:09 +0600

    Add and remove HTML tag by js

--- Exercise-14 (Create, Find, Add, Remove html)/script.js
+++ Exercise-14 (Create, Find, Add, Remove html)/script.js
@@ -11,3 +11,13 @@ heading2.appendChild(text);
 
 var myDiv = document.getElementById("my-div");
 myDiv.appendChild(heading2);
+
+//adding element in specific position
+var heading0 = document.createElement("h3");
+var text0 = document.createTextNode("This is creation H3 Tag by Javascript.");
+heading0.appendChild(text0);
+myDiv.insertBefore(heading0,heading1);
+
+//Remove Element/Tag
+var heading3 = document.getElementsByTagName("h1")[1];
+myDiv.removeChild(heading3);
