63f4ff2453f8ec8e7410862749b4118a48190b55
atikrpi27 <atik.rpi27@gmail.com>
2022-02-28 23:29:39 +0600

    Ex-16: Changing CSS Property

---
+++ Exercise-16 (Changing CSS Property)/script.js
@@ -0,0 +1,18 @@
+var p1 = document.querySelector("#p1");
+var p2 = document.querySelector("#p2");
+
+//add css property
+function add(){
+    p1.classList.add("p1");
+}
+function add1(){
+    p2.classList.add("p2");
+}
+
+//Remove css property
+function remove(){
+    p1.classList.remove("p1");
+}
+function remove1(){
+    p2.classList.remove("p2");
+}
