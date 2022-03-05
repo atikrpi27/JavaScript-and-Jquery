ca105ae7d19b542fb484a213daad8ca20204a9e9
atikrpi27 <atik.rpi27@gmail.com>
2022-03-05 23:25:45 +0600

    Ex-20: Keypress Listener

---
+++ Exercise-20 (Keypress Listener)/script.js
@@ -0,0 +1,13 @@
+//keypress listener
+
+document.addEventListener("keypress", function(event){
+    var txt = event.key;
+    document.querySelector("p").innerHTML = ("You have pressed " + txt);
+});
+
+count = 0;
+document.querySelector("textarea").addEventListener("keypress", function(event){
+    var txt = event.key;
+    count++;
+    document.querySelector("h6").innerHTML = ("You have pressed " + count +" charecters.");
+});
