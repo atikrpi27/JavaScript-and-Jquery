de10ccd5dc23b66547a75f0404b186039e54a151
atikrpi27 <atik.rpi27@gmail.com>
2022-03-09 23:17:16 +0600

    DOM OnChange event in Input Field

--- Exercise-21 (DOM event- OnChange)/script.js
+++ Exercise-21 (DOM event- OnChange)/script.js
@@ -2,15 +2,15 @@ const input = document.querySelector("input");
 input.addEventListener("change", changeHanderel);
 
 function changeHanderel(event){
-    // console.log(event);
-    // document.write(event);
-    // document.write(event.type);
-    // document.write(event.target);
-    // console.log(event.target);
-    // console.log(event.target.id);
-    // document.write(event.target.id);
-    // document.write(event.target.name);
-    // console.log(event.target.name);
-    document.write(event.target.value);
+    console.log(event);
+    document.write(event+"<br>");
+    document.write(event.type+"<br>");
+    document.write(event.target+"<br>");
+    console.log(event.target);
+    console.log(event.target.id);
+    document.write(event.target.id+"<br>");
+    document.write(event.target.name+"<br>");
+    console.log(event.target.name);
+    document.write(event.target.value+"<br>");
     console.log(event.target.value);
 };
