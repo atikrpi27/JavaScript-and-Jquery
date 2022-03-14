4e37067bcd1a4b66a5f6a18949fdae8d7613ed49
atikrpi27 <atik.rpi27@gmail.com>
2022-03-14 23:31:15 +0600

    Ex-24 DOM Load and Unload Event

---
+++ Exercise-24 (DOM  event)/script.js
@@ -0,0 +1,13 @@
+//load event
+
+window.addEventListener("load", function (){
+    // document.write("Load");
+    console.log("load");
+});
+
+//unload event
+
+window.addEventListener("unload", function (){
+    console.log("Unload");
+});
+
