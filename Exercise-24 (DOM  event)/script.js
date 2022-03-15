286ca16ec2c72b244ebc30164ac1105d027abc7b
atikrpi27 <atik.rpi27@gmail.com>
2022-03-15 22:23:42 +0600

    Screen resize and Toggle event

--- Exercise-24 (DOM  event)/script.js
+++ Exercise-24 (DOM  event)/script.js
@@ -17,4 +17,17 @@ window.addEventListener("scroll", function (){
     console.log("scrolled");
 });
 
+//Screen resize event
 
+window.addEventListener("resize", function(){
+    const width = window.outerWidth;
+    const height = window.outerHeight;
+    console.log(`Height: ${height}, Width: ${width}`);
+});
+
+//Toggle event
+
+const details = document.querySelector("details");
+details.addEventListener("toggle", function(){
+    console.log("Toggle");
+});
