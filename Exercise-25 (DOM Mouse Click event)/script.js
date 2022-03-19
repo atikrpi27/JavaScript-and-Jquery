a98e5f5b842be586c9e30627b25f0df5b01706b7
atikrpi27 <atik.rpi27@gmail.com>
2022-03-19 23:25:02 +0600

    Ex-25 DOM Mouse Click event

---
+++ Exercise-25 (DOM Mouse Click event)/script.js
@@ -0,0 +1,9 @@
+var div = document.querySelector("div");
+
+div.addEventListener("click", function(){
+    console.log("Click Event is occured.");
+});
+
+div.addEventListener("dblclick", function(){
+    console.log("Dobule Click Event is occured.");
+});
