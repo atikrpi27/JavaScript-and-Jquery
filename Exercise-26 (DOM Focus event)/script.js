12a6dc45f3323c40fbec9ce0794598badb61bc96
atikrpi27 <atik.rpi27@gmail.com>
2022-04-12 23:32:32 +0600

    Add style and lower case to Upper case

--- Exercise-26 (DOM Focus event)/script.js
+++ Exercise-26 (DOM Focus event)/script.js
@@ -1,11 +1,14 @@
 const Inputfocus = document.querySelector("input");
 
-Inputfocus.addEventListener("blur", function(){
-    console.log("Blur Event is occured.");
+Inputfocus.addEventListener("blur", function(e){
+    // console.log("Blur Event is occured.");
+    Inputfocus.style.backgroundColor = "white";
+    Inputfocus.value = e.target.value.toUpperCase();
 });
 
 Inputfocus.addEventListener("focus", function(){
-    console.log("Focus Event is occured.");
+    // console.log("Focus Event is occured.");
+    Inputfocus.style.backgroundColor = "Yellow";
 });
 
 Inputfocus.addEventListener("focusin", function(){
