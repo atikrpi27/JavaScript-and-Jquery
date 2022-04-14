eb8d8d0ca39100a9d58b4c0431b4b95613c36944
atikrpi27 <atik.rpi27@gmail.com>
2022-04-14 22:53:24 +0600

    Output using innerHtml in Copy,Cut,Paste DOM event

--- Exercise-27 (DOM Clipboard event)/script.js
+++ Exercise-27 (DOM Clipboard event)/script.js
@@ -1,13 +1,17 @@
 const InputSelector = document.querySelector("input");
+const PeragraphSelector = document.querySelector("p");
 
 InputSelector.addEventListener("copy", function(){
     console.log("You have copped!");
+    PeragraphSelector.innerHTML = ("You have copped!");
 });
 
 InputSelector.addEventListener("cut", function(){
     console.log("You have cutted!");
+    PeragraphSelector.innerHTML = ("You have cutted!");
 });
 
 InputSelector.addEventListener("paste", function(){
     console.log("You have pasted!");
+    PeragraphSelector.innerHTML = ("You have pasted!");
 });
