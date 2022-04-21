821fbdf78c0ab8fc9ad3944ba2dd974eae6c9118
atikrpi27 <atik.rpi27@gmail.com>
2022-04-21 21:58:44 +0600

    Location properties uaing BOM and DOM

--- Exercise-29 (BOM event)/script.js
+++ Exercise-29 (BOM event)/script.js
@@ -16,3 +16,23 @@ console.log(location.port);
 
 //pathname
 console.log(location.pathname);
+
+
+
+//from index file
+const locationDiv = document.querySelector(".location-div");
+
+const p1 = locationDiv.children[0];
+p1.textContent = location.href;
+
+const p2 = locationDiv.children[1];
+p2.textContent = location.protocol;
+
+const p3 = locationDiv.children[2];
+p3.textContent = location.hostname;
+
+const p4 = locationDiv.children[3];
+p4.textContent = location.port;
+
+const p5 = locationDiv.children[4];
+p5.textContent = location.pathname;
