a446b7c050c77b1f2cb614632baca80a0118f0e8
atikrpi27 <atik.rpi27@gmail.com>
2022-09-17 01:20:37 +0600

    unique Id and Action Button added

--- projects/Todo List/script.js
+++ projects/Todo List/script.js
@@ -1,24 +1,32 @@
 //selector
+
 const container = document.querySelector('#container');
 const todoForm = document.querySelector('#todoForm');
 const todoInput = document.querySelector('#todoInput');
 const todoBbtn = document.querySelector('#todoBbtn');
 const todoList = document.querySelector('#list');
 
-
 //create Element
-const createTodo = (todoValue,todoId)=>{
+const createTodo = (todoValue, todoId) => {
     const todoNewRow = document.createElement('tr');
     const todoElement = document.createElement('td');
+    todoElement.id = todoId;
+    console.log(todoElement);
     todoElement.innerHTML = `
-        <span>${todoValue}</span>
+        <div class="d-flex d-row justify-content-between">
+            <span>${todoValue}</span>
+            <div class="action">
+                <button type="submit" class="btn btn-success">Edit</button>
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </div>
+        </div>
     `
     todoNewRow.appendChild(todoElement);
     todoList.appendChild(todoNewRow);
 }
 
 //create addTodo
-const addTodo = (e)=>{
+const addTodo = (e) => {
     e.preventDefault();
     const todoValue = todoInput.value;
 
@@ -26,7 +34,8 @@ const addTodo = (e)=>{
     const todoId = Date.now().toString();
 
     createTodo(todoValue, todoId);
-    
+
+    todoForm.reset(); //for clear From input filed after submitting
 }
 
 //Add Listener
