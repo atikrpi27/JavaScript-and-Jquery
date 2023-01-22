c42d6d2188f835510e92e16cdb2a48c7276bd4f9
Md. Atikur Rahman <atik.rpi27@gmail.com>
2023-01-22 09:41:25 +0600

    Add Todo List

--- JQuery/JavaScript Practice/todo list/script.js
+++ JQuery/JavaScript Practice/todo list/script.js
@@ -2,14 +2,31 @@ var input = document.querySelector('#userInput');
 var enter = document.querySelector('#enter');
 var ul = document.querySelector('ul')
 
-enter.addEventListener('click', function(){
-    if (input.value != ""){
-        var list = document.createElement('li');        // Create new List
-        list.innerText =  input.value;                  // Put input field value in List
-        ul.appendChild(list)                            // Append List in UnOrderList
-        input.value= "";                                // Reset input field
+enter.addEventListener('click', function () {
+    if (input.value != "") {
+        createElement();
     }
-    else{
+    else {
         alert("Please enter your todo...")
     }
 })
+
+function createElement() {
+    var list = document.createElement('li');        // Create new List
+
+    // Create Edit, Delete Button and put input field value in list variable
+    list.innerHTML = `                             
+            <div class="d-flex d-row justify-content-between">
+                <span>${input.value}</span>
+                <div class="action">
+                    <button type="submit" class="btn btn-success" id="editBtn">Edit</button>
+                    <button type="submit" class="btn btn-danger" id="deleteBtn">Delete</button>
+                </div>
+            </div>
+        `
+
+    ul.appendChild(list)                            // Append List in UnOrderList
+    input.value = "";                                // Reset input field
+
+}
+
