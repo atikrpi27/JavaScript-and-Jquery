023bdeb43f55fededb0a50c20f778d7509d35eba
Md. Atikur Rahman <atik.rpi27@gmail.com>
2023-01-24 15:45:27 +0600

    Edit Todo Function added

--- JQuery/JavaScript Practice/todo list/jqurey.js
+++ JQuery/JavaScript Practice/todo list/jqurey.js
@@ -2,17 +2,17 @@ $(document).ready(function () {
     let i = 0;
     $('#enter').click(function () {
         let todoVal = $('#userInput').val()
-        
-        if (todoVal != "") {            
+
+        if (todoVal != "") {
             i++;
-            var a = $('.listItems-ul').append(`<li class="d-flex d-row justify-content-between" id= ${i}>
+            $('.listItems-ul').append(`<li class="d-flex d-row justify-content-between" id= ${i}>
                 <span>${todoVal}</span>
                 <div class="action">
-                    <button type="submit" class="btn btn-success" id="editBtn">Edit</button>
+                    <button type="submit" class="btn btn-success editBtn" id="editBtn">Edit</button>
                     <button type="submit" class="btn btn-danger deleteBtn" id="${i}">Delete</button>
                 </div>
                 </li>`);
-                $('#userInput').val('');              
+            $('#userInput').val('');
         }
         else {
             alert("Please enter your todo...")
@@ -20,15 +20,35 @@ $(document).ready(function () {
 
         // Delete Function 
         $('.deleteBtn').click(function () {
-            $(this).parent().parent().remove()                      // Remove Normally parent wise
+            // $(this).parent().parent().remove()                   // Remove Normally parent wise
 
-            if ($(this).id == $(this).parent().parent().id){        //  Remove using Id
+            if ($(this).id == $(this).parent().parent().id) {        //  Remove using Id
                 $(this).parent().parent().remove();
             }
             else {
                 alert("Please match your id")
             }
         })
+
+        // Edit Function
+        $('.editBtn').click(function () {
+            let editParent = $(this).parent().parent().find('span');
+            let oldValue = editParent.text();
+            editParent.html('<input id="uInput" type="text" placeholder="Edit Item..." />')
+            $(this).html('Save')
+            $(this).removeClass('editBtn').addClass('saveBtn');
+            let uInput = $('#uInput').val(oldValue);
+
+        $('.saveBtn').click(function () {
+            let newValue = uInput.val();
+            editParent.html(`<span>${newValue}</span>`)
+            $(this).html('Edit')
+            $(this).removeClass('saveBtn').addClass('editBtn');
+        })
+
+        })
+
+
     })
 
 
