cf7093dc9c6733b22d7f832270ce806c950fd1f4
Md. Atikur Rahman <atik.rpi27@gmail.com>
2023-01-26 13:47:49 +0600

    Control buttons on owl carousel by js

--- JQuery/JavaScript Practice/owl carousel/script.js
+++ JQuery/JavaScript Practice/owl carousel/script.js
@@ -1,11 +1,41 @@
-$(function() {
+var leftBtn = document.querySelector('.left');
+var rightBtn = document.querySelector('.right');
+
+$(function () {
     var owl = $(".owl-carousel");
     owl.owlCarousel({
-      items:4,
-      loop:true,
-      margin:7,
-      autoplay:true,
-      autoplayTimeout:4000,
-      autoplayHoverPause:true
+        items: 4,
+        loop: true,
+        margin: 10,
+        autoplay:true,
+        autoplayTimeout: 2000,
+        // nav : true,
+        // dots : false,
+        autoplayHoverPause: true,
     });
-  });
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.querySelector(".item");
+    console.log(slides.length)
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
+
