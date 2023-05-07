16de3affe7ab4a8b9a2a422c2580a6cb34916491
Md. Atikur Rahman <58600348+atikrpi27@users.noreply.github.com>
2023-05-07 23:30:53 +0600

    update datepicker

--- JQuery/jQuery/DatePicker/script.js
+++ JQuery/jQuery/DatePicker/script.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
     $(function(){
-        $('#datePicker').datepicker();
+        $('#datePicker').datepicker("setDate", '22/03/2020')
+        $('#datePicker').datepicker({
+            // defaultDate: "22/03/2020",
+            dateFormat: 'dd-mm-yy',
+            changeMonth: true,
+            changeYear: true,
+            // numberOfMonths:2,
+            showButtonPanel: true,
+            labelMonthNext: 'Next month',
+            labelMonthSelect: 'Select a month',
+            // minDate: new Date(),    //for disable prev months and date
+        });
     })
-
     $('.btn-submit').click(function(){
         let inputValue = $('#datePicker').val();
         console.log(inputValue);
