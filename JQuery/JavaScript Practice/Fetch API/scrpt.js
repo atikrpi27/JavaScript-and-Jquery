ad0bfa88f93451fc73a8e5ed4208ac2a1e078923
Md. Atikur Rahman <58600348+atikrpi27@users.noreply.github.com>
2023-06-15 23:52:38 +0600

    fetch API data and display using event

--- JQuery/JavaScript Practice/Fetch API/scrpt.js
+++ JQuery/JavaScript Practice/Fetch API/scrpt.js
@@ -1,21 +1,22 @@
 
 const btn = document.querySelector('#btn');
+const btn1 = document.querySelector('#btn1');
 const display = document.querySelector("#show");
+const display1 = document.querySelector("#show1");
 
-// btn.addEventListener('click', function () {
-//     fetch('https://jsonplaceholder.typicode.com/todos/2')
-//         .then(res => res.text())
-//         .then(data => {
-//             display.innerText = data;
-//     })
-// })
+btn.addEventListener('click', function () {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(res => res.text())
+        .then(data => {
+            display.innerText = data;
+            console.log(data);
+    })
+})
 
-// data()
-
-btn.addEventListener('click', async function () {
+btn1.addEventListener('click', async function () {
     const route = await fetch('https://jsonplaceholder.typicode.com/todos/2')
     const data = await route.text();
     console.log(data);
-    display.innerText = data;
+    display1.innerText = data;
 })
 
