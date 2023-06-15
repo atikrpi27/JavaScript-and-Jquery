548eae4148b627a30c885bdb06f83bf3eb7df122
Md. Atikur Rahman <58600348+atikrpi27@users.noreply.github.com>
2023-06-15 23:48:28 +0600

    update API fetch data

--- JQuery/JavaScript Practice/Fetch API/scrpt.js
+++ JQuery/JavaScript Practice/Fetch API/scrpt.js
@@ -2,10 +2,20 @@
 const btn = document.querySelector('#btn');
 const display = document.querySelector("#show");
 
-btn.addEventListener('click',function(){
-    fetch('https://jsonplaceholder.typicode.com/todos/2')
-    .then(res => res.text())
-    .then(data => {
-        display.innerText = data;
-    })
+// btn.addEventListener('click', function () {
+//     fetch('https://jsonplaceholder.typicode.com/todos/2')
+//         .then(res => res.text())
+//         .then(data => {
+//             display.innerText = data;
+//     })
+// })
+
+// data()
+
+btn.addEventListener('click', async function () {
+    const route = await fetch('https://jsonplaceholder.typicode.com/todos/2')
+    const data = await route.text();
+    console.log(data);
+    display.innerText = data;
 })
+
