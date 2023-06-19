ef276f02457d7befba522159b68ca45945342d9e
Md. Atikur Rahman <58600348+atikrpi27@users.noreply.github.com>
2023-06-19 23:29:12 +0600

    higher order callback function

--- Exercise-34 (Callback And Higher Order Function)/higherOrderFunction.js
+++ Exercise-34 (Callback And Higher Order Function)/higherOrderFunction.js
@@ -1,24 +1,35 @@
-const functionOne = () => {
+const functionOne = (callback) => {
     console.log("This is First function");
+    callback();
 }
-async function functionTwo() {          //asynchronous function
+async function functionTwo(callback) {              //asynchronous function
     setTimeout(function() {
         console.log("This is Second function");
+        callback();
     }, 2000);
 
 }
-const functionThree = () => {
+const functionThree = (callback) => {
     console.log("This is Third function");
+    callback();
 }
-const functionFour = () => {
+const functionFour = (callback) => {
     console.log("This is Fourth function");
+    callback();
 }
 const functionFive = () => {
     console.log("This is Five function");
 }
 
-functionOne();
-functionTwo();
-functionThree();
-functionFour();
-functionFive();
+console.log('Hi this is callback function start here.')
+
+functionOne(() => {
+    functionTwo(() => {
+        functionThree(() => {
+            functionFour(() => {
+                functionFive()
+                console.log('Hi this is callback function end here.')
+            })
+        })
+    })
+});
